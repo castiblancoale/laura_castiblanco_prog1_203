9d7a4cc5a2511703aa15e97f02d5199bcd38c716
castiblancoale <lauraalejandra2416@gmail.com>
2025-02-14 14:44:04 -0500

    Primer versión

---
+++ Intro/operadores2.py
@@ -0,0 +1,4 @@
+x=int(input("ingrese un valor en pulgadas"))
+print(x)
+r=x*2.54
+print("en centimetros =",r)
---
+++ Intro/operadores3.py
@@ -0,0 +1,6 @@
+x=int(input("ingrese un valor en celcius"))
+print(x)
+k=x+273.15
+f=(x*9/5)+32
+print("en kelvins =",k)
+print("en fahrenheit =", f)
